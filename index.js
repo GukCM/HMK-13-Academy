f6c5194ffa137fdc3a45be987669f278255f528b
Gustavo Ceballos Miranda <gceballos6@ucol.mx>
2024-04-04 17:27:07 -0600

    Last minute changes

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-const iterateElements = (className, callback) => {
-    const elements = document.getElementsByClassName(className);
-    for (let index = 0; index < elements.length; index++) {
-        callback(elements[index]);
+const loopFAQItems = (className, callback) => {
+    const faqItems = document.getElementsByClassName(className);
+    for (let index = 0; index < faqItems.length; index++) {
+        callback(faqItems[index]);
     }
 };
 
-const changeActive = (element) => {
-    if (element.classList.contains("active")) {
-        iterateElements('cont-question', (el) => el.classList.remove('active'));
-        iterateElements('question-title', (el) => el.classList.remove('active'));
-        iterateElements('arrow-down', (el) => el.classList.remove('active'));
-        iterateElements('question-answer', (el) => el.classList.remove('active'));
+const changeActiveFAQ = (element) => {
+    if (element.classList.contains('active')) {
+        loopFAQItems('faq-question-cont', (el) => el.classList.remove('active'));
+        loopFAQItems('faq-question-title', (el) => el.classList.remove('active'));
+        loopFAQItems('faq-arrow-down', (el) => el.classList.remove('active'));
+        loopFAQItems('faq-question-answer', (el) => el.classList.remove('active'));
     } else {
-        iterateElements('cont-question', (el) => el.classList.remove('active'));
+        loopFAQItems('faq-question-cont', (el) => el.classList.remove('active'));
         element.classList.add('active');
-        
-        iterateElements('question-title', (el) => el.classList.remove('active'));
-        iterateElements('arrow-down', (el) => el.classList.remove('active'));
-        iterateElements('question-answer', (el) => el.classList.remove('active'));
-        
-        const title = element.getElementsByClassName('question-title');
-        const arrow = element.getElementsByClassName('arrow-down');
-        const answer = element.getElementsByClassName('question-answer');
-        
+   
+        loopFAQItems('faq-question-title', (el) => el.classList.remove('active'));
+        loopFAQItems('faq-arrow-down', (el) => el.classList.remove('active'));
+        loopFAQItems('faq-question-answer', (el) => el.classList.remove('active'));
+    
+        const title = element.getElementsByClassName('faq-question-title');
+        const arrow = element.getElementsByClassName('faq-arrow-down');
+        const answer = element.getElementsByClassName('faq-question-answer');
         title[0].classList.add('active');
         arrow[0].classList.add('active');
         answer[0].classList.add('active');
     }
 }
 
-const initializeAccordion = () => {
-    iterateElements('cont-question', (el) => 
-        el.addEventListener('click', () => changeActive(el))
+const initFAQ = () => {
+    loopFAQItems('faq-question-cont', (el) => 
+        el.addEventListener('click', () => changeActiveFAQ(el))
     );
 };
 
-initializeAccordion();
+initFAQ();
